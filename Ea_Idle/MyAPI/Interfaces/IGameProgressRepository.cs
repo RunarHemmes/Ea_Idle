c5bbc187522eb5f9c432bc9f64a1f00852450ff4
Runar <s1208862@student.windesheim.nl>
2026-04-13 15:40:47 +0200

    Added a model, repository and interface for the game progress.

---
+++ Ea_Idle/MyAPI/Interfaces/IGameProgressRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyAPI.Models;
+
+namespace MyAPI.Interfaces
+{
+    public interface IGameProgressRepository
+    {
+        public List<GameProgress> GetAll();
+
+        public GameProgress? Get(int id);
+
+        public GameProgress Add(GameProgress progress);
+
+        public GameProgress? Update(GameProgress progress);
+
+        public GameProgress? Delete(GameProgress progress);
+    }
+}
