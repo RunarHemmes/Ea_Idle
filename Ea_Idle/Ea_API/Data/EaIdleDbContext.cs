1ea2012a0df63ff85be9dca064044e139b1506b8
Runar <s1208862@student.windesheim.nl>
2026-04-14 18:14:22 +0200

    Created database. Created the sqlite database file using entity framework. Made EaIdleDbContext to make this possible.

---
+++ Ea_Idle/Ea_API/Data/EaIdleDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ea_API.Models;
+
+namespace Ea_API.Data
+{
+    public class EaIdleDbContext(DbContextOptions<EaIdleDbContext> options) : DbContext(options)
+    {
+        public DbSet<Account> Accounts { get; set; }
+        
+        public DbSet<GameProgress> GameProgresses { get; set; }
+    }
+}
---
+++ Ea_Idle/Ea_API/Migrations/20260414160828_Initial_DB_Create.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ea_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_DB_Create : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameProgresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SilverPennies = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameProgresses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "GameProgresses");
+        }
+    }
+}
