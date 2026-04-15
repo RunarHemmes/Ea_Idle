39795b0475f9410f22a55c9ed33008310d1383bd
Runar <s1208862@student.windesheim.nl>
2026-04-15 14:31:38 +0200

    Added JSON Web Token authorization. (I hope)

--- Ea_Idle/Ea_API/Program.cs
+++ Ea_Idle/Ea_API/Program.cs
@@ -1,17 +1,39 @@
 using Microsoft.EntityFrameworkCore;
 using Ea_API.Data;
 using Ea_API.IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 builder.Services.AddRepositories();
 builder.Services.AddControllers();
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<EaIdleDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("APIDbCS")));
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+// JWT auth.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
