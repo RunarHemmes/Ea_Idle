0723fabbde99d9c6bb50e22e16945a03ecfd95d1
Runar <s1208862@student.windesheim.nl>
2026-04-26 11:46:50 +0200

    Added MapGet's, so page refresh works.

--- Ea_Idle/EaIdleWebsite/Program.cs
+++ Ea_Idle/EaIdleWebsite/Program.cs
@@ -1,7 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Make sure the browser doesn't get a 404 on page refresh:
 app.MapGet("/", () => Results.File("index.html", "text/html"));
+app.MapGet("/Mining", () => Results.File("index.html", "text/html"));
+app.MapGet("/Tharni", () => Results.File("index.html", "text/html"));
+app.MapGet("/Castar", () => Results.File("index.html", "text/html"));
+app.MapGet("/Silmaril", () => Results.File("index.html", "text/html"));
+app.MapGet("/Help", () => Results.File("index.html", "text/html"));
+app.MapGet("/Settings", () => Results.File("index.html", "text/html"));
+app.MapGet("/Credits", () => Results.File("index.html", "text/html"));
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
