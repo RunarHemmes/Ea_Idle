17836e2b936919755397496f127d8d9389bd4133
Runar <s1208862@student.windesheim.nl>
2026-05-09 15:11:22 +0200

    Added the repository and interface for the connections table.

---
+++ Ea_Idle/Ea_API/Interfaces/IConnectionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ea_API.Models;
+
+namespace Ea_API.Interfaces
+{
+    public interface IConnectionRepository
+    {
+        public Connection? GetByParent(int parentId);
+
+        public Connection? GetByChild(int childId);
+
+        public Connection Add(Connection connection);
+
+        public Connection? Update(Connection connection);
+
+        public Connection? Delete(Connection connection);
+
+
+    }
+}
---
+++ Ea_Idle/Ea_API/Repositories/ConnectionRepository.cs
@@ -0,0 +1,84 @@
+﻿using Ea_API.Data;
+using Ea_API.Interfaces;
+using Ea_API.Models;
+
+namespace Ea_API.Repositories
+{
+    public class ConnectionRepository : IConnectionRepository
+    {
+        private readonly EaIdleDbContext _context;
+
+        public ConnectionRepository(EaIdleDbContext context)
+        {
+            _context = context;
+        }
+
+        public Connection? GetByParent(int parentId)
+        {
+            try
+            {
+                Connection connect = _context.Connections.Single(c => c.ParentId == parentId);
+                return connect;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public Connection? GetByChild(int childId)
+        {
+            try
+            {
+                Connection connect = _context.Connections.Single(c => c.ChildId == childId);
+                return connect;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public Connection Add(Connection connection)
+        {
+            try
+            {
+                _context.Add(connection);
+                _context.SaveChanges();
+                return connection;
+            }
+            catch
+            {
+                throw new Exception("Something went wrong when adding a new GameProgress.");
+            }
+        }
+
+        public Connection? Update(Connection connection)
+        {
+            try
+            {
+                _context.Update(connection);
+                _context.SaveChanges();
+                return connection;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public Connection? Delete(Connection connection)
+        {
+            try
+            {
+                _context.Remove(connection);
+                _context.SaveChanges();
+                return connection;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
