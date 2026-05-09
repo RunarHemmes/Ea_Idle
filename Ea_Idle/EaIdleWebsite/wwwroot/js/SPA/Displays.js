5b557c89b54cf7c9e3293494f83cda20385efc04
Runar <s1208862@student.windesheim.nl>
2026-05-09 22:31:00 +0200

    Made only 1 settings page at a time show, and made it selectable.

--- Ea_Idle/EaIdleWebsite/wwwroot/js/SPA/Displays.js
+++ Ea_Idle/EaIdleWebsite/wwwroot/js/SPA/Displays.js
@@ -10,6 +10,12 @@ class Displays {
 
         window.addEventListener("mining.html", this.MiningLoaded.bind(this));
         window.addEventListener("login.html", this.LoginLoaded.bind(this));
+        window.addEventListener("tharni.html", this.RemoveAllListeners.bind(this));
+        window.addEventListener("castar.html", this.RemoveAllListeners.bind(this));
+        window.addEventListener("silmaril.html", this.RemoveAllListeners.bind(this));
+        window.addEventListener("help.html", this.RemoveAllListeners.bind(this));
+        window.addEventListener("settings.html", this.SettingsLoaded.bind(this));
+        window.addEventListener("credits.html", this.RemoveAllListeners.bind(this));
     }
 
     RemoveNavBar() {
@@ -35,9 +41,44 @@ class Displays {
         this.UpdateSpDisplay();
     }
 
+    SettingsLoaded() {
+        this.RemoveAllListeners();
+        document.getElementById("Settings_General").addEventListener("click", this.LoadGeneralSettings.bind(this))
+        document.getElementById("Settings_Account").addEventListener("click", this.LoadAccountSettings.bind(this))
+        document.getElementById("Settings_Parental").addEventListener("click", this.LoadParentSettings.bind(this))
+        this.LoadGeneralSettings();
+    }
+
     LoginLoaded() {
         this.RemoveNavBar();
         //document.getElementById("Login_Btn").addEventListener("click", app.Login.bind(app))
+    }
+
+    LoadGeneralSettings() {
+        this.HideSettingsPages();
+        document.getElementById("General_Page").hidden = false;
+        document.getElementById("General_Page").classList.remove("Hidden");
+    }
+
+    LoadAccountSettings() {
+        this.HideSettingsPages();
+        document.getElementById("Account_Page").hidden = false;
+        document.getElementById("Account_Page").classList.remove("Hidden");
+    }
+
+    LoadParentSettings() {
+        this.HideSettingsPages();
+        document.getElementById("Parent_Page").hidden = false;
+        document.getElementById("Parent_Page").classList.remove("Hidden");
+    }
+
+    HideSettingsPages() {
+        document.getElementById("General_Page").hidden = true;
+        document.getElementById("General_Page").classList.add("Hidden");
+        document.getElementById("Account_Page").hidden = true;
+        document.getElementById("Account_Page").classList.add("Hidden");
+        document.getElementById("Parent_Page").hidden = true;
+        document.getElementById("Parent_Page").classList.add("Hidden");
     }
 
     UpdateSpDisplay() {
