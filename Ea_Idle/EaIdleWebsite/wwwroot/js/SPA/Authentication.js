78588c50cd1090d988dfa3ad8da4b1fddfdaaa32
Runar <s1208862@student.windesheim.nl>
2026-05-12 18:23:25 +0200

    Added input validation for the registration form.

--- Ea_Idle/EaIdleWebsite/wwwroot/js/SPA/Authentication.js
+++ Ea_Idle/EaIdleWebsite/wwwroot/js/SPA/Authentication.js
@@ -4,10 +4,13 @@
     passwordField
     usernameField
     errorLbl
+    emailField
+    confirmField
 
     constructor(accountAPI) {
         this.accountAPI = accountAPI;
         window.addEventListener("login.html", this.LoginLoaded.bind(this));
+        window.addEventListener("registration.html", this.RegistrationLoaded.bind(this));
     }
 
     LoginLoaded() {
@@ -26,7 +29,7 @@
         
         const username = this.usernameField.value;
         const password = this.passwordField.value;
-        let inputStatus = this.ValidateInput(username, password);
+        const inputStatus = this.ValidateLoginInput(username, password);
 
         if (inputStatus != "Ok") {
             this.errorLbl.innerText = inputStatus;
@@ -39,11 +42,55 @@
         window.dispatchEvent(new CustomEvent("LoggedIn"));
     }
 
-    ValidateInput(name, pass) {
+    ValidateLoginInput(name, pass) {
         if (name == "" || pass == "") {
             return "Please fill in all fields";
         }
         return "Ok"; 
+    }
+
+    RegistrationLoaded() {
+        this.btn = document.getElementById("Registration_Btn");
+        this.usernameField = document.getElementById("Username_Input");
+        this.emailField = document.getElementById("Email_Input");
+        this.passwordField = document.getElementById("Password_Input");
+        this.confirmField = document.getElementById("Confirm_Input");
+        this.errorLbl = document.getElementById("Registration_Error");
+
+        this.btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            this.Register();
+        })
+    }
+
+    async Register() {
+        const username = this.usernameField.value;
+        const email = this.emailField.value;
+        const password = this.passwordField.value;
+        const confirm = this.confirmField.value;
+        const inputStatus = this.ValidateRegisterInput(username, email, password, confirm);
+
+        if (inputStatus != "Ok") {
+            this.errorLbl.innerText = inputStatus;
+        }
+        
+    }
+
+    ValidateRegisterInput(name, mail, pass, confirm) {
+        if (name == "" || mail == "" || pass == "" || confirm == "") {
+            return "Please fill in all fields";
+        }
+        if (7 < pass.lenght < 16) {
+            return "Your password is the wrong length, it should be 8-5 characters";
+        }
+        if (pass != confirm) {
+            return "The passwords are not the same";
+        }
+        let mailCheck = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (mailCheck.test(mail)) {
+            return "This email is not valid, please check for mistakes";
+        }
+        return "Ok";
     }
 }
 
