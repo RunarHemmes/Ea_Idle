b9248794e1e81808ba888d6d7ead65a66967dd72
Runar <s1208862@student.windesheim.nl>
2026-05-26 14:29:27 +0200

    Added the Mining Upgrades to the Game Progres table. Also made all the default values for them in the GameProgress object.

--- Ea_Idle/Ea_API/Models/GameProgress.cs
+++ Ea_Idle/Ea_API/Models/GameProgress.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Ea_API.Models
 {
@@ -11,6 +12,8 @@ namespace Ea_API.Models
 
         public string SilverPennies { get; set; }
 
+        public Dictionary<string, Dictionary<string, int>> MiningUpgrades { get; set; }
+
         public GameProgress(int accountId)
         {
             AccountId = accountId;
@@ -20,6 +23,33 @@ namespace Ea_API.Models
         public void SetDefault()
         {
             SilverPennies = "0";
+            MiningUpgrades = new Dictionary<string, Dictionary<string, int>>
+            {
+                { "Equipment", new Dictionary<string, int> {
+                    { "Lvl", 1},
+                    { "Price", 10},
+                    { "Current bonus", 0},
+                    { "Bonus mult", 98}
+                } },
+                { "Miners Count", new Dictionary<string, int> {
+                    { "Lvl", 1},
+                    { "Price", 250},
+                    { "Current bonus", 0},
+                    { "Bonus mult", 50}
+                } },
+                { "Ore Purity", new Dictionary<string, int> {
+                    { "Lvl", 1},
+                    { "Price", 20},
+                    { "Current bonus", 0},
+                    { "Bonus add", 1}
+                } },
+                { "Ore Price", new Dictionary<string, int> {
+                    { "Lvl", 1},
+                    { "Price", 20},
+                    { "Current bonus", 0},
+                    { "Bonus add", 1}
+                } },
+            };
         }
     }
 }
