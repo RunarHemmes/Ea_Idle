3b63aa1292775e1e699a5db8bdc976649d232f6a
Runar <s1208862@student.windesheim.nl>
2026-05-26 20:12:18 +0200

    Added the html and css for the upgrades and set the default levels to 0.

--- Ea_Idle/Ea_API/Models/GameProgress.cs
+++ Ea_Idle/Ea_API/Models/GameProgress.cs
@@ -26,25 +26,25 @@ namespace Ea_API.Models
             MiningUpgrades = new Dictionary<string, Dictionary<string, int>>
             {
                 { "Equipment", new Dictionary<string, int> {
-                    { "Lvl", 1},
+                    { "Lvl", 0},
                     { "Price", 10},
                     { "Current bonus", 0},
                     { "Bonus mult", 98}
                 } },
                 { "Miners Count", new Dictionary<string, int> {
-                    { "Lvl", 1},
+                    { "Lvl", 0},
                     { "Price", 250},
                     { "Current bonus", 0},
                     { "Bonus mult", 50}
                 } },
                 { "Ore Purity", new Dictionary<string, int> {
-                    { "Lvl", 1},
+                    { "Lvl", 0},
                     { "Price", 20},
                     { "Current bonus", 0},
                     { "Bonus add", 1}
                 } },
                 { "Ore Price", new Dictionary<string, int> {
-                    { "Lvl", 1},
+                    { "Lvl", 0},
                     { "Price", 20},
                     { "Current bonus", 0},
                     { "Bonus add", 1}
