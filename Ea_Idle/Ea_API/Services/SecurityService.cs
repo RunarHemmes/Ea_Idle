a5f3f2e0b77600aa22b0deb098b764e8feab2ec5
Runar <s1208862@student.windesheim.nl>
2026-05-26 14:57:04 +0200

    Added inputvalidation for the new upgrades.

--- Ea_Idle/Ea_API/Services/SecurityService.cs
+++ Ea_Idle/Ea_API/Services/SecurityService.cs
@@ -77,6 +77,7 @@ namespace Ea_API.Services
         public (bool succes, string? message) ValidateProgressValues(GameProgress progress)
         {
             string sp = progress.SilverPennies;
+            Dictionary<string, Dictionary<string, int>> mu = progress.MiningUpgrades;
             if (string.IsNullOrWhiteSpace(sp))
             {
                 return (false, "Important information is missing!");
@@ -84,6 +85,24 @@ namespace Ea_API.Services
             if (sp.Contains('-')) 
             {
                 return (false, "Silver Pennies amount can not be negative.");
+            }
+            var expectedStructure = new Dictionary<string, HashSet<string>>
+            {
+                { "Equipment", new HashSet<string> { "Lvl", "Price", "Current bonus", "Bonus mult" } },
+                { "Miners Count", new HashSet<string> { "Lvl", "Price", "Current bonus", "Bonus mult" } },
+                { "Ore Purity", new HashSet<string> { "Lvl", "Price", "Current bonus", "Bonus add" } },
+                { "Ore Price", new HashSet<string> { "Lvl", "Price", "Current bonus", "Bonus add" } }
+            };
+            if (expectedStructure.Count != mu.Count ||
+                !expectedStructure.Keys.ToHashSet().SetEquals(mu.Keys))
+                return (false, "Important information is missing!");
+            foreach (var (outerKey, innerDict) in mu)
+            {
+                if (!expectedStructure.TryGetValue(outerKey, out var expectedInnerKeys))
+                    return (false, "Important information is missing!");
+
+                if (!expectedInnerKeys.SetEquals(innerDict.Keys))
+                    return (false, "Important information is missing!");
             }
             return (true, null);
         }
