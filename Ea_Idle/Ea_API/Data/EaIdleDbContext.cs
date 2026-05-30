78e0a1f328bf821ad88932492a43d0fd64ed6b93
Runar <s1208862@student.windesheim.nl>
2026-05-30 21:23:04 +0200

    Put the new hashed password in the start accounts in the DB.

--- Ea_Idle/Ea_API/Data/EaIdleDbContext.cs
+++ Ea_Idle/Ea_API/Data/EaIdleDbContext.cs
@@ -14,8 +14,8 @@ namespace Ea_API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Account>().HasData(
-                new(1, "Harold", "passwordHarold", "Harold@mail.com", "Player", 123456),
-                new(2, "John", "passwordJohn", "John@mail.com", "Parent", 666666)
+                new(1, "Harold", "fSP67HucoSVmtUinGSITVQ==.tceWYgZT5EE3khRFS1Y5/L6A8K3GhUyvqUHxiNFcnXU=", "Harold@mail.com", "Player", 123456),
+                new(2, "John", "qB2uM3X3+C1wob9GYHZy3A==./Fmbnk5d3hpyoopv0/9/Nsb5kfMAjbP5yWAFo6+xb7o=", "John@mail.com", "Parent", 666666)
                 );
         }
     }
